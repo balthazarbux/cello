21a31e7c3500e69f27e2552618a9a4457ce5a725
Joe Curlee <curlee@fastmail.com>
2016-04-03 23:22:02 -0500

    added test for parts_from_csv

--- tools/pycello/tests.py
+++ tools/pycello/tests.py
@@ -1,10 +1,10 @@
 import unittest
 
-from ucf_writer import gates_from_csv
+from ucf_writer import gates_from_csv, parts_from_csv
 
 
-class TestGatesFromCSV(unittest.TestCase):
-    def test_method_returns_expected_value(self):
+class TestUCFWriterMethods(unittest.TestCase):
+    def test_gates_from_csv_returns_expected_value(self):
         expected_gates = [
             {
                 'collection': 'gates',
@@ -48,6 +48,72 @@ class TestGatesFromCSV(unittest.TestCase):
         gates = gates_from_csv(table, header_map)
 
         self.assertEqual(gates, expected_gates)
+
+    def test_parts_from_csv_returns_expected_value(self):
+        expected_parts = [
+            {
+                'collection': 'parts',
+                'type': 'ribozyme',
+                'name': 'foo',
+                'dnasequence': '0101'
+            },
+            {
+                'collection': 'parts',
+                'type': 'rbs',
+                'name': 'foo2',
+                'dnasequence': '01012'
+            },
+            {
+                'collection': 'parts',
+                'type': 'cds',
+                'name': 'foo3',
+                'dnasequence': '01013'
+            },
+            {
+                'collection': 'parts',
+                'type': 'terminator',
+                'name': 'foo4',
+                'dnasequence': '01014'
+            },
+            {
+                'collection': 'parts',
+                'type': 'promoter',
+                'name': 'foo5',
+                'dnasequence': '01015'
+            },
+        ]
+
+        table = [
+            {
+                'asdf': 'foo',
+                'zxcv': '0101',
+                'asdf2': 'foo2',
+                'zxcv2': '01012',
+                'asdf3': 'foo3',
+                'zxcv3': '01013',
+                'asdf4': 'foo4',
+                'zxcv4': '01014',
+                'asdf5': 'foo5',
+                'zxcv5': '01015'
+            }
+        ]
+
+        header_map = {
+            'ribozyme': 'asdf',
+            'ribozymeDNA': 'zxcv',
+            'rbs': 'asdf2',
+            'rbsDNA': 'zxcv2',
+            'cds': 'asdf3',
+            'cdsDNA': 'zxcv3',
+            'terminator': 'asdf4',
+            'terminatorDNA': 'zxcv4',
+            'promoter': 'asdf5',
+            'promoterDNA': 'zxcv5'
+        }
+
+        parts = parts_from_csv(table, header_map)
+
+        self.assertEqual(parts, expected_parts)
 
 if __name__ == '__main__':
     unittest.main()
